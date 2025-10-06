f9e086beb16b9d2cebdbeaf521b4454be4213407
pozercpp <n.a.chigrin@yandex.ru>
2025-10-06 22:56:30 +0300

    Update pipe.h

--- include/pipe.h
+++ include/pipe.h
@@ -8,3 +8,6 @@ int PipeCreate(int fd[2]);
 int PipeWrite(int, const void*, size_t);
 int PipeRead(int, void*, size_t);
 void PipeClose(int);
+
+void PipeRedirect(int, int);
+void ProcessExecute(const char*, const char*);
